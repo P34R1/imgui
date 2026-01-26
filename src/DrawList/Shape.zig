c39a23e6c3d4bd034d65d4b619d4f99ddbae9fa9
pearl <vincent.fortin279@gmail.com>
2026-01-26 09:23:34 -0500

    simplify shapes into one file and rearrange parameters

--- src/DrawList/Shape.zig
+++ src/DrawList/Shape.zig
@@ -4,13 +4,14 @@ const c = @import("c");
 
 const imgui = @import("../imgui.zig");
 const Vec2 = imgui.Vec2;
-pub const Circle = @import("Circle.zig");
-pub const Ellipse = @import("Ellipse.zig");
-pub const Line = @import("Line.zig");
-pub const Ngon = @import("Ngon.zig");
-pub const Quad = @import("Quad.zig");
-pub const Rect = @import("Rect.zig");
-pub const Triangle = @import("Triangle.zig");
+const shapes = @import("shapes.zig");
+pub const Circle = shapes.Circle;
+pub const Triangle = shapes.Triangle;
+pub const Ellipse = shapes.Ellipse;
+pub const Line = shapes.Line;
+pub const Ngon = shapes.Ngon;
+pub const Quad = shapes.Quad;
+pub const Rect = shapes.Rect;
 
 /// `null` means filled shape.
 thickness: ?f32,
@@ -18,14 +19,6 @@ inner: union(enum) { line: Line, rect: Rect, triangle: Triangle, quad: Quad, cir
 
 pub fn line(p1: Vec2, p2: Vec2, thickness: f32) @This() {
     return @This(){ .thickness = thickness, .inner = .{ .line = .{ .p1 = p1, .p2 = p2 } } };
-}
-
-pub fn rect(min: Vec2, max: Vec2, thickness: ?f32, ex: Rect.Ex) @This() {
-    return @This(){ .thickness = thickness, .inner = .{ .rect = .{ .min = min, .max = max, .ex = ex } } };
-}
-
-pub fn filledRect(min: Vec2, max: Vec2, ex: Rect.Ex) @This() {
-    return rect(min, max, null, ex);
 }
 
 pub fn triangle(p1: Vec2, p2: Vec2, p3: Vec2, thickness: ?f32) @This() {
@@ -36,6 +29,14 @@ pub fn filledTriangle(p1: Vec2, p2: Vec2, p3: Vec2) @This() {
     return triangle(p1, p2, p3, null);
 }
 
+pub fn rect(min: Vec2, max: Vec2, ex: Rect.Ex, thickness: ?f32) @This() {
+    return @This(){ .thickness = thickness, .inner = .{ .rect = .{ .min = min, .max = max, .ex = ex } } };
+}
+
+pub fn filledRect(min: Vec2, max: Vec2, ex: Rect.Ex) @This() {
+    return rect(min, max, ex, null);
+}
+
 pub fn quad(p1: Vec2, p2: Vec2, p3: Vec2, p4: Vec2, thickness: ?f32) @This() {
     return @This(){ .thickness = thickness, .inner = .{ .quad = .{ .p1 = p1, .p2 = p2, .p3 = p3, .p4 = p4 } } };
 }
@@ -44,20 +45,20 @@ pub fn filledQuad(p1: Vec2, p2: Vec2, p3: Vec2, p4: Vec2) @This() {
     return quad(p1, p2, p3, p4, null);
 }
 
-pub fn circle(center: Vec2, radius: f32, thickness: ?f32, ex: Circle.Ex) @This() {
+pub fn circle(center: Vec2, radius: f32, ex: Circle.Ex, thickness: ?f32) @This() {
     return @This(){ .thickness = thickness, .inner = .{ .circle = .{ .center = center, .radius = radius, .ex = ex } } };
 }
 
 pub fn filledCircle(center: Vec2, radius: f32, ex: Circle.Ex) @This() {
-    return circle(center, radius, null, ex);
+    return circle(center, radius, ex, null);
 }
 
-pub fn ellipse(center: Vec2, radius: Vec2, thickness: ?f32, ex: Ellipse.Ex) @This() {
+pub fn ellipse(center: Vec2, radius: Vec2, ex: Ellipse.Ex, thickness: ?f32) @This() {
     return @This(){ .thickness = thickness, .inner = .{ .ellipse = .{ .center = center, .radius = radius, .ex = ex } } };
 }
 
 pub fn filledEllipse(center: Vec2, radius: Vec2, ex: Ellipse.Ex) @This() {
-    return ellipse(center, radius, null, ex);
+    return ellipse(center, radius, ex, null);
 }
 
 pub fn ngon(center: Vec2, radius: f32, num_segments: c_int, thickness: ?f32) @This() {
---
+++ src/DrawList/shapes.zig
@@ -0,0 +1,66 @@
+const std = @import("std");
+
+const c = @import("c");
+
+const imgui = @import("../imgui.zig");
+const Vec2 = imgui.Vec2;
+
+test {
+    std.testing.refAllDecls(@This());
+}
+
+pub const Line = struct {
+    p1: Vec2,
+    p2: Vec2,
+};
+
+pub const Triangle = struct {
+    p1: Vec2,
+    p2: Vec2,
+    p3: Vec2,
+};
+
+pub const Rect = struct {
+    min: Vec2,
+    max: Vec2,
+    ex: Ex,
+
+    pub const Ex = struct {
+        rounding: f32 = 0,
+        flags: c.ImDrawFlags = 0,
+    };
+};
+
+pub const Quad = struct {
+    p1: Vec2,
+    p2: Vec2,
+    p3: Vec2,
+    p4: Vec2,
+};
+
+pub const Circle = struct {
+    center: Vec2,
+    radius: f32,
+    ex: Ex,
+
+    pub const Ex = struct {
+        num_segments: c_int = 0,
+    };
+};
+
+pub const Ellipse = struct {
+    center: Vec2,
+    radius: Vec2,
+    ex: Ex,
+
+    pub const Ex = struct {
+        rot: f32 = 0,
+        num_segments: c_int = 0,
+    };
+};
+
+pub const Ngon = struct {
+    center: Vec2,
+    radius: f32,
+    num_segments: c_int,
+};
